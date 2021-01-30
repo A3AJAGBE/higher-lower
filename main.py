28b10b0c59238631b5b80ba5633bd83344cc5cd8
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-30 07:57:01 +0000

    Add accounts selection

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
 """
 A game application that compares two social media following. The user has to guess which account is higher or lower
 """
+
+# Imports
+from data import data
+import random
+
+
+def game():
+
+    # selecting the accounts
+    compare = random.choice(data)
+    name = compare['name']
+    desc = compare['description']
+    country = compare['country']
+    print(f"A: {name}, a {desc}, from {country}")
+
+    against = random.choice(data)
+    if against == compare:
+        against = random.choice(data)
+    name = against['name']
+    desc = against['description']
+    country = against['country']
+    print(f"B: {name}, a {desc}, from {country}")
+
+
+game()
