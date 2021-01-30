45ef2e2c1d6f6901fd0cfedd16a455aa5694b24e
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-30 08:25:20 +0000

    Add while loop

--- main.py
+++ main.py
@@ -9,42 +9,47 @@ import random
 
 def game():
 
-    # selecting the accounts
-    compare = random.choice(data)
-    name = compare['name']
-    desc = compare['description']
-    country = compare['country']
-    print(f"A: {name}, a {desc}, from {country}")
+    game_over = False
+    while not game_over:
+        # selecting the accounts
+        compare = random.choice(data)
+        name = compare['name']
+        desc = compare['description']
+        country = compare['country']
+        print(f"\nA: {name}, a {desc}, from {country}")
 
-    against = random.choice(data)
-    if against == compare:
         against = random.choice(data)
-    name = against['name']
-    desc = against['description']
-    country = against['country']
-    print(f"B: {name}, a {desc}, from {country}")
+        if against == compare:
+            against = random.choice(data)
+        name = against['name']
+        desc = against['description']
+        country = against['country']
+        print(f"B: {name}, a {desc}, from {country}")
 
-    A = compare["follower_count"]
-    B = against["follower_count"]
+        A = compare["follower_count"]
+        B = against["follower_count"]
 
-    # Testing, will be removed
-    print(A)
-    print(B)
+        # Testing, will be removed
+        print(A)
+        print(B)
 
-    # User guess
-    guess = input('Who has more followers? "A" or "B"\n').upper()
+        # User guess
+        guess = input('Who has more followers? "A" or "B"\n').upper()
 
-    # Compare followers
-    if guess == 'A' and A > B:
-        print('Correct, A is the answer.')
-    elif guess == 'A' and B > A:
-        print('Wrong, B is the answer.')
-    elif guess == 'B' and A < B:
-        print('Correct, B is the answer.')
-    elif guess == 'B' and A > B:
-        print('Wrong, A is the answer.')
-    else:
-        print('Invalid guess.')
+        # Compare followers
+        if guess == 'A' and A > B:
+            print('Correct, A is the answer.')
+        elif guess == 'A' and B > A:
+            print('Wrong, B is the answer.')
+            game_over = True
+        elif guess == 'B' and A < B:
+            print('Correct, B is the answer.')
+        elif guess == 'B' and A > B:
+            print('Wrong, A is the answer.')
+            game_over = True
+        else:
+            print('Invalid guess.')
+            game_over = True
 
 
 game()
