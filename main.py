5794d110cfc5ba7e1f5fadd4fe29dd50e44e14eb
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-01-30 09:22:47 +0000

    Refactor the code

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ A game application that compares two social media following. The user has to gue
 # Imports
 import os
 from data import data
+from logo import logo
 import random
 
 
@@ -14,32 +15,28 @@ def clear():
 
 
 def game():
+    print(logo)
     # Initial game score
     score = 0
+    first_acc = random.choice(data)
 
     game_over = False
     while not game_over:
-
         # Display number of correct guess
-        print(f'\nCurrent score: {score}\n')
+        print(f'Current score: {score}\n')
 
         # selecting the accounts
-        compare = random.choice(data)
-        name = compare['name']
-        desc = compare['description']
-        country = compare['country']
-        print(f"A: {name}, a {desc}, from {country}")
+        second_acc = random.choice(data)
 
-        against = random.choice(data)
-        if against == compare:
-            against = random.choice(data)
-        name = against['name']
-        desc = against['description']
-        country = against['country']
-        print(f"B: {name}, a {desc}, from {country}")
+        if first_acc == second_acc:
+            second_acc = random.choice(data)
+        else:
+            print(f"A: {first_acc['name']}, a {first_acc['description']}, from {first_acc['country']}")
 
-        A = compare["follower_count"]
-        B = against["follower_count"]
+            print(f"B: {second_acc['name']}, a {second_acc['description']}, from {second_acc['country']}")
+
+        A = first_acc["follower_count"]
+        B = second_acc["follower_count"]
 
         # Testing, will be removed
         print(A)
@@ -48,11 +45,15 @@ def game():
         # User guess
         guess = input('Who has more followers? "A" or "B"\n').upper()
         clear()
+        print(logo)
 
         # Compare followers
         if guess == 'A' and A > B:
             print('Correct, A is the answer.')
             score += 1
+            # Replace the correct guess has the first account
+            guess_data = first_acc
+            first_acc = guess_data
         elif guess == 'A' and B > A:
             print('Wrong, B is the answer.')
             print(f'\nFinal score: {score}\n')
@@ -60,6 +61,9 @@ def game():
         elif guess == 'B' and A < B:
             print('Correct, B is the answer.')
             score += 1
+            # Replace the correct guess has the first account
+            guess_data = second_acc
+            first_acc = guess_data
         elif guess == 'B' and A > B:
             print('Wrong, A is the answer.')
             print(f'\nFinal score: {score}\n')
